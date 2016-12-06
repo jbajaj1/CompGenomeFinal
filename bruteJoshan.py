25e84f38a1ea1598ab0f7085400d580281dad4fe
Joshan Bajaj <jbajaj1@jhu.edu>
2016-12-06 17:04:35 -0500

    Brute force method, not tested yet

---
+++ bruteJoshan.py
@@ -0,0 +1,84 @@
+import fileinput
+import sys
+import os
+import getopt
+import pickle
+
+
+numA = 0
+numC = 0
+numG = 0
+numT = 0
+numN = 0
+INVERSION_LEN = 40
+READ_LEN = 100
+def findMicroInversions(read):
+	position = 0
+	while position + INVERSION_LEN < len(read):
+		numA = 0
+		numC = 0
+		numG = 0
+		numT = 0
+		numN = 0
+		revComp = reverseCompliment(read, position)
+		if (numA, numC, numG, numT, numN) in sequencesDic:
+			for element in sequencesDic[(numA, numC, numG, numT, numN)]:
+				if sequence[element:element+READ_LEN] == revComp:
+					print("The following contains a microinversion: " + sequence[element:element+READ_LEN])
+					print("It matches with the read that starts at position " + str(element))
+					print("The microinversion occurs at character position " + str(position))
+				
+
+
+def reverseCompliment(read, position):
+	revComp = read[0:position]
+	for char in revComp:
+		if char == "A":
+			numA += 1
+		elif char == "C":
+			numC += 1
+		elif char == "G":
+			numG += 1 
+		elif char == "T":
+			numT += 1
+		elif char == "N":
+			numN += 1
+	temprev = read[position:position+INVERSION_LEN]
+	temprev = temprev[::-1]
+	for char in temprev:
+		if char == "A": 
+			revComp += "T"
+			numT += 1
+		elif char == "C":
+			revComp += "G"
+			numG += 1
+		elif char == "G":
+			revComp += "C"
+			numC += 1
+		elif char == "T":
+			revComp += "A"
+			numA += 1
+		elif char == "N":
+			revComp += "N"
+			numN += 1
+	revComp += read[position+INVERSION_LEN]
+	for char in read[position+INVERSION_LEN:]:
+		if char == "A":
+			numA += 1
+		elif char == "C":
+			numC += 1
+		elif char == "G":
+			numG += 1 
+		elif char == "T":
+			numT += 1
+		elif char == "N":
+			numN += 1
+	return revComp
+
+
+
+sequencesDic = pickle.load(open("sequencesDic.p", "rb"))
+sequence = pickle.load(open("seqString.p", "rb"))
+for line in sys.stdin:
+	line.strip("\n")
+	findMicroInversions(line.upper())
