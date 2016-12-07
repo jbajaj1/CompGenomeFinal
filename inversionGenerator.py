cc36bc747a88ccc4241d73ad58f0e09b43fb87a3
John <johnd.wilson9@gmail.com>
2016-12-07 16:12:50 -0500

    added global variables for flexibility

--- inversionGenerator.py
+++ inversionGenerator.py
@@ -3,6 +3,11 @@
 import sys
 import random
 
+READ_LENGTH = 10
+NUM_READS = 2
+MIN_INVERSION_LENGTH = 15
+MAX_INVERSION_LENGTH = 100
+
 def parse_input():
     data = ""
     sys.stdin.readline()
@@ -10,7 +15,24 @@ def parse_input():
         data += line.strip().upper()
     return data
 
-def generate_inversions(seq, numInversions, minLen, maxLen, readStart):
+def simulate_reads():
+    global READ_LENGTH
+    global NUM_READS
+
+    reads = []
+    indecies = []
+    
+    for i in range(NUM_READS):
+        readIndex = random.randint(0, len(data) - READ_LENGTH)
+        read = data[readIndex:readIndex + READ_LENGTH]
+        reads.append(read)    
+        indecies.append(readIndex)
+    return reads, indecies
+
+def generate_inversions(seq, numInversions, readStart):
+    global MIN_INVERSION_LENGTH
+    global MAX_INVERSION_LENGTH
+    
     compliments = {'A':'T','T':'A','C':'G','G':'C'} # Reverse complement map
     invertedSegments = [] # Keep track of inverted segments to prevent overlapping inversions
 
@@ -19,37 +41,31 @@ def generate_inversions(seq, numInversions, minLen, maxLen, readStart):
         
         while (collision):
             collision = False
-            inversionLen = random.randint(minLen, maxLen)
+            inversionLen = random.randint(MIN_INVERSION_LENGTH, MAX_INVERSION_LENGTH)
             inversionIndex = random.randint(0, len(seq) - inversionLen)
             
             for prevInversion in invertedSegments:
                 if ((inversionIndex <= prevInversion[0] and inversionIndex + inversionLen >= prevInversion[0]) or (inversionIndex <= prevInversion[1] and inversionIndex + inversionLen >= prevInversion[1])):
                     collision = True
-#                    print("Collision!")
 
-        print("Read from index %d to %d in the sequence with a %dbp microinversion from %d to %d" % (readStart, readStart + 100, inversionLen, inversionIndex, inversionIndex + 4))
+        print("Read from index %d to %d in the sequence with a %dbp microinversion from %d to %d" % (readStart, readStart + READ_LENGTH, inversionLen, inversionIndex, inversionIndex + 4))
         originalSeq = seq[inversionIndex:inversionIndex + inversionLen]
         invertedSegments.append((inversionIndex, inversionIndex + inversionLen))
-#print("Inversion %d: index = %d, len = %d" % (i, inversionIndex, inversionLen))
         
         invertedSegment = ""
         for n in range(1, inversionLen + 1):
             invertedSegment += compliments[originalSeq[-n]]
 
-    return seq[:inversionIndex] + invertedSegment + seq[inversionIndex + inversionLen:]
+    return seq[:inversionIndex] + invertedSegment + seq[inversionIndex + inversionLen:], inversionLen
 
 data = parse_input()
-#data = "ACGTTTTTTA" # Hardcoded example sequence
-#readIndex = random.randint(100, len(data) - 100)
-readIndex = 10
-read = data[readIndex:readIndex + 10]
+reads, indecies = simulate_reads()
 
-invertedRead = generate_inversions(read, 1, 4, 4, readIndex)
-#print("Read with 40bp micro inversion: %s" % invertedRead)
-print(invertedRead)
-print("Normal read from 0 to 10")
-print(data[:10])
-#print
-#print(data[:20])
-#print(data[100:200])
+for read, index in reads, indecies:
+    invertedRead, inversionLen = generate_inversions(read, 1, index)
+    print("Read/Read with %dbp micro inversion" % inversionLen)
+    print(read)
+    print(invertedRead)
+
+
 
