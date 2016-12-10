9ac48da01bacfb7cb785bd6100522d726b9c66e3
Oneeb Malik <oneebmalik@Oneebs-MacBook-Pro.local>
2016-12-09 23:58:09 -0500

    modify run.sh file

--- submission/run.sh
+++ submission/run.sh
@@ -2,4 +2,4 @@
 python generateSeq.py > seq.txt
 python preprocessSeq.py < seq.txt
 python inversionGenerator.py < seq.txt > reads.txt
-python bruteJoshan.py < reads.txt
+python findMicroInversions.py < reads.txt
