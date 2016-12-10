e8a6717ef80037373cad7c60e67940fbc9f4e672
Dan Smillie <dansmillie96@gmail.com>
2016-12-09 20:40:36 -0500

    commented findMicroInversions.py

--- submission/findMicroInversions.py
+++ submission/findMicroInversions.py
@@ -5,6 +5,7 @@ import getopt
 import pickle
 import time
 
+#Initialize global variables
 numA = 0
 numC = 0
 numG = 0
@@ -14,8 +15,10 @@ invN = 0
 INVERSION_LEN = 100
 MIN_INVERSION = 15
 READ_LEN = 100
+#Threshold for how many N's in the read we will handle
 MAX_N = 2
 found = False
+#function to detect microinversion from a read
 def findMicroInversions(read):
 	global numG
 	global numA
@@ -28,8 +31,10 @@ def findMicroInversions(read):
 	global INVERSION_LEN
 	found = False
 	INVERSION_LEN = 100
+	#loop to try all inversion sizes starting from INVERSION_LEN
 	while INVERSION_LEN >= MIN_INVERSION:
 		position = 0
+		#loop to check all of the possible inversions of the given length
 		while position + INVERSION_LEN <= READ_LEN:
 			numA = 0
 			numC = 0
@@ -37,14 +42,18 @@ def findMicroInversions(read):
 			numT = 0
 			numN = 0
 			invN = 0
+			#generate reverse Compliment of the read given the position we are starting the inversion
 			revComp = reverseCompliment(read, position)
+			#if there are too many N's we throw out that position
 			if invN > MAX_N:
 				position += 1
 				break
+			#if we need to handle any N's
 			elif invN > 0 and invN <= MAX_N:
 				result = nHandling(revComp, revComp, invN, numA, numC, numG, numT, numN, invN, position)
 				if found is True:
 					return 0
+			#try all the hits from the preprocessed dictionary.
 			for element in sequencesDic.get((numA, numC, numG, numT, numN), []):
 				if sequence[element:element+READ_LEN].strip("\n") == revComp.strip("\n"):
 					print("The following contains a microinversion: " + read.strip("\n"))
@@ -53,7 +62,7 @@ def findMicroInversions(read):
 					print("The microinversion occurs at character position " + str(position) + " in the read")
 					print("It matches to: " + sequence[element:element+READ_LEN])
 					return 0
-			position += 1	
+			position += 1
 		INVERSION_LEN -= 1
 	'''
 	VERSION WITHOUT VARYING INVERSION LENGTH
@@ -77,7 +86,7 @@ def findMicroInversions(read):
 		position += 1
 	'''
 
-
+#recursive function to handle the case where a read has N's in it
 def nHandling(read, unchangedRead, Ns, As, Cs, Gs, Ts, otherNs, origNs, position):
 	global found
 	global numA
@@ -86,6 +95,7 @@ def nHandling(read, unchangedRead, Ns, As, Cs, Gs, Ts, otherNs, origNs, position
 	global numT
 	if found == True:
 		return 0
+	#recurse on the N being an A,C,G, or T
 	if Ns > 0:
 		nHandling(read[0:position] + read[position:position+INVERSION_LEN].replace("N", "A", 1) + read[position+INVERSION_LEN:], unchangedRead, Ns - 1, As + 1, Cs, Gs, Ts, otherNs - 1, origNs, position)
 		nHandling(read[0:position] + read[position:position+INVERSION_LEN].replace("N", "C", 1) + read[position+INVERSION_LEN:], unchangedRead, Ns - 1, As, Cs + 1, Gs, Ts, otherNs - 1, origNs, position)
@@ -93,6 +103,7 @@ def nHandling(read, unchangedRead, Ns, As, Cs, Gs, Ts, otherNs, origNs, position
 		nHandling(read[0:position] + read[position:position+INVERSION_LEN].replace("N", "T", 1) + read[position+INVERSION_LEN:], unchangedRead, Ns - 1, As, Cs, Gs, Ts + 1, otherNs - 1, origNs, position)
 	if found == True:
 		return 0
+	#check the dictionary hits against the converted string
 	for element in sequencesDic.get((As, Cs, Gs, Ts, otherNs), []):
 		if sequence[element:element+READ_LEN].strip("\n") == read.strip("\n"):
 			print("The following may contain a microinversion: " + unchangedRead.strip("\n"))
@@ -117,7 +128,7 @@ def nHandling(read, unchangedRead, Ns, As, Cs, Gs, Ts, otherNs, origNs, position
 	if found == True:
 		return 0
 
-
+#generate the reverse complement and count the number of A,C,G,&T
 def reverseCompliment(read, position):
 	global numG
 	global numA
@@ -133,7 +144,7 @@ def reverseCompliment(read, position):
 		elif char == "C":
 			numC += 1
 		elif char == "G":
-			numG += 1 
+			numG += 1
 		elif char == "T":
 			numT += 1
 		elif char == "N":
@@ -141,7 +152,7 @@ def reverseCompliment(read, position):
 	temprev = read[position:position+INVERSION_LEN]
 	temprev = temprev[::-1]
 	for char in temprev:
-		if char == "A": 
+		if char == "A":
 			revComp += "T"
 			numT += 1
 		elif char == "C":
@@ -164,18 +175,17 @@ def reverseCompliment(read, position):
 		elif char == "C":
 			numC += 1
 		elif char == "G":
-			numG += 1 
+			numG += 1
 		elif char == "T":
 			numT += 1
 		elif char == "N":
 			numN += 1
 	return revComp
 
-
+#time the run
 start = time.time()
 sequencesDic = pickle.load(open("sequencesDic.p", "rb"))
 sequence = pickle.load(open("seqString.p", "rb"))
-#start = time.time()   <--- Talk to team about this
 for line in sys.stdin:
 	line.strip("\n")
 	startRead = time.time()
